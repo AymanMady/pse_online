83b0fd46504e45d7de529bd7404183e116d447f1
Abdo <130005951+AbdoLegrand@users.noreply.github.com>
2024-01-16 17:19:19 +0000

    install typed js

--- admin/acceuil.php
+++ admin/acceuil.php
@@ -25,6 +25,19 @@ include_once "nav_bar.php";
         </li>
       </ul>
     </nav>
+
   </div>
+    <h3 class="text-center mt-5">بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</h3><br>
+
+    <span class="text-center mt-5" id="element"></span>
+
+<script src="../node_modules/typed.js/dist/typed.umd.js"></script>
+<script>
+  var typed = new Typed('#element', {
+    strings: ['<h4>﴾ يَاأَيُّهَا الَّذِينَ آمَنُوا لَا تَسْأَلُوا عَنْ أَشْيَاءَ إِنْ تُبْدَ لَكُمْ تَسُؤْكُمْ  وَإِنْ تَسْأَلُوا عَنْهَا حِينَ يُنَزَّلُ الْقُرْآنُ تُبْدَ لَكُم عَفَا اللَّهُ عَنْهَا وَاللَّهُ غَفُورٌ حَلِيمٌ ﴿</h4>'],
+    typeSpeed: 10,
+    // loop: true,
+  });
+</script>
 </div>
 </div>
