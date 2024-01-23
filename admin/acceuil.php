17cde8266fccaace61c81d5ace088f4da800573d
Abdo <130005951+AbdoLegrand@users.noreply.github.com>
2024-01-23 14:05:19 +0000

    Commit Abdo

--- admin/acceuil.php
+++ admin/acceuil.php
@@ -1,43 +1,32 @@
 <?php
-session_start() ;
+session_start();
 $email = $_SESSION['email'];
-if($_SESSION["role"]!="admin"){
-    header("location:../authentification.php");
+if ($_SESSION["role"] != "admin") {
+  header("location:../authentification.php");
 }
 ?>
 <title>Acceuil</title>
 
-<?php 
+<?php
 include_once "nav_bar.php";
 ?>
 
 <div class="main-panel">
-<div class="content-wrapper">
-  <div class="page-header">
-    <h3 class="page-title">
-      <span class="page-title-icon bg-gradient-primary text-white me-2">
-        <i class="mdi mdi-home"></i>
-      </span> Accueil
-    </h3>
-    <nav aria-label="breadcrumb">
-      <ul class="breadcrumb">
-        <li class="breadcrumb-item active" aria-current="page">
-        </li>
-      </ul>
-    </nav>
+  <div class="content-wrapper">
+    <div class="page-header">
+      <h3 class="page-title">
+        <span class="page-title-icon bg-gradient-primary text-white me-2">
+          <i class="mdi mdi-home"></i>
+        </span> Accueil
+      </h3>
+      <nav aria-label="breadcrumb">
+        <ul class="breadcrumb">
+          <li class="breadcrumb-item active" aria-current="page">
+          </li>
+        </ul>
+      </nav>
+
+    </div>
 
   </div>
-    <h3 class="text-center mt-5"><b>بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</b></h3><br>
-
-    <span class="text-center mt-5" id="element"></span>
-
-<script src="../node_modules/typed.js/dist/typed.umd.js"></script>
-<script>
-  var typed = new Typed('#element', {
-    strings: ['<h4><b>﴾ يَاأَيُّهَا الَّذِينَ آمَنُوا لَا تَسْأَلُوا عَنْ أَشْيَاءَ إِنْ تُبْدَ لَكُمْ تَسُؤْكُمْ  وَإِنْ تَسْأَلُوا عَنْهَا حِينَ يُنَزَّلُ الْقُرْآنُ تُبْدَ لَكُم عَفَا اللَّهُ عَنْهَا وَاللَّهُ غَفُورٌ حَلِيمٌ ﴿</b></h4>'],
-    typeSpeed: 10,
-    // loop: true,
-  });
-</script>
-</div>
 </div>
